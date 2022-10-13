a9c54c278f442208aa064e5b381e60e6150c3899
Alejandro Aguilar <alejandroaguilardev>
2022-10-13 14:18:44 -0500

    logos actualizados

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,71 +1,31 @@
-import Image from 'next/image';
-import classes from './footer.module.css';
+import Image from "next/image";
+import { imagesLogo } from "./data";
+import classes from "./footer.module.css";
 
 export const Footer = () => {
-    return (
-        <div className={classes.container}>
-            <div className={classes.logo}>
-                <a
-                    href='https://qolkrex.foundation/'
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
+	return (
+		<div className={classes.container}>
+			<div className={classes.logo}>
+				{imagesLogo.map((data) => (
+					<a
+						key={data.alt}
+						href={data.href}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<Image
+							src={data.src}
+							width={data.width}
+							height={data.height}
+							alt={data.alt}
+						/>
+					</a>
+				))}
+			</div>
 
-                    <Image
-                        src='/img/logos/estrella-andina.png'
-                        width={140}
-                        height={40}
-                        alt="Estrella Andina"
-                    />
-                </a>
-
-                <a
-                    href='https://celo.org/es'
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-
-                    <Image
-                        src='/img/logos/celo-logo-color-reverse.png'
-                        width={150}
-                        height={40}
-                        alt="Celo"
-                    />
-                </a>
-
-                
-                <a
-                    href='https://www.linkedin.com/company/coini-app/'
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-
-                    <Image
-                        src='/img/logos/coini.png'
-                        width={150}
-                        height={40}
-                        alt="coini"
-                    />
-                </a>
-
-                
-                <a
-                    href='#'
-                    // target="_blank"
-                    // rel="noopener noreferrer"
-                >
-
-                    <Image
-                        src='/img/logos/comandante.png'
-                        width={140}
-                        height={40}
-                        alt="comandante"
-                    />
-                </a>
-            </div>
-
-            <h3 className={classes.copyrigth}>© 2021-{new Date().getFullYear()} Qolkrex - All Rights Reserved.</h3>
-
-        </div>
-    )
-}
+			<h3 className={classes.copyrigth}>
+				© 2021-{new Date().getFullYear()} Qolkrex - All Rights Reserved.
+			</h3>
+		</div>
+	);
+};
---
+++ src/components/Footer/data.ts
@@ -0,0 +1,37 @@
+export const imagesLogo = [
+	{
+		href: "https://qolkrex.foundation/",
+		src: "/img/logos/estrella-andina.png",
+		width: 130,
+		height: 50,
+		alt: "Estrella Andina",
+	},
+	{
+		href: "https://celo.org/es",
+		src: "/img/logos/celo.png",
+		width: 160,
+		height: 50,
+		alt: "Celo",
+	},
+	{
+		href: "https://www.linkedin.com/company/coini-app/",
+		src: "/img/logos/coini.png",
+		width: 140,
+		height: 40,
+		alt: "Coini",
+	},
+	{
+        href: "https://www.instagram.com/metaburgernomas/?hl=es",
+		src: "/img/logos/meta-burger.png",
+		width: 140,
+		height: 50,
+		alt: "meta burger",
+	},
+    {
+        href: "#",
+        src: "/img/logos/comandante.png",
+        width: 80,
+        height: 60,
+        alt: "comandante",
+    },
+];
